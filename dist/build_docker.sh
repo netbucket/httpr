6f10dbbd18976cb81a94f01a18dc44d331e02cbe
Igor Bondarenko <igor.bondarenko@ibm.com>
2017-07-12 10:41:05 -0400

    Updated Docker image tag

--- dist/build_docker.sh
+++ dist/build_docker.sh
@@ -3,7 +3,7 @@
 # Build the Docker image for httpr
 #
 HTTPR_IMAGE="netbucket/httpr"
-HTTPR_VERSION="0.1.3"
+HTTPR_VERSION="1.0.0"
 
 docker build dist -f dist/Dockerfile -t $HTTPR_IMAGE:$HTTPR_VERSION -t $HTTPR_IMAGE:latest
 docker push $HTTPR_IMAGE:$HTTPR_VERSION
